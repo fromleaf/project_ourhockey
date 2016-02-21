3bb07d8d6ee21d0aa643862431cad3bb96297e92
yoon <fromleaf@gmail.com>
2016-02-21 15:16:41 +0900

    Player Detail UI bug fixed

--- ourhockey/ourhockey_playermanager/urls.py
+++ ourhockey/ourhockey_playermanager/urls.py
@@ -3,5 +3,5 @@ from ourhockey_playermanager import views
 
 urlpatterns = [
     url(r'^$', views.PlayerListView.as_view(), name='players_list'),
-    url(r'^players/(?P<pk>\d+)/$', views.PlayerDetailView.as_view(), name='players_detail'),
+    url(r'^players/(?P<pk>\d+)/$', views.PlayerDetailView.as_view(), name='player_detail'),
 ]
